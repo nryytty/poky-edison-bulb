3e5edd01d0030936975071b6b779da6542059898
Niko@Home <niko.ryytty@gmail.com>
2015-02-01 15:20:48 +0200

    Poky cmake project template for Intel Edison projects slowly shaping up

---
+++ src/bulb.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main (int argc, char **argv)
+{
+    printf("“To invent, you need a good imagination and a pile of junk.” \n -Thomas A. Edison \n");
+    return 0;
+}
---
+++ src/bulb.pc.cmake
@@ -0,0 +1,10 @@
+prefix=@CMAKE_INSTALL_PREFIX@
+exec_prefix=${prefix}
+libdir=${exec_prefix}/lib@LIB_SUFFIX@
+includedir=${prefix}/include
+
+Name: @PROJECT_NAME@
+Description: CMake project template for Intel Edison projects
+Version: @project_VERSION_STRING@
+
+
