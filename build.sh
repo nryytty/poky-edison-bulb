33e3f60ffc4dd8f3db43af2e2a078015e142bf22
Niko@Home <niko.ryytty@gmail.com>
2015-02-01 14:11:38 +0200

    removed library building

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo ""
+echo "============================================"
+echo "Bulp - Project template for Intel Edison"
+echo "============================================"
+
+source /opt/poky-edison/1.6/environment-setup-core2-32-poky-linux
+mkdir build
+cd build
+cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/Toolchains/oe-sdk_cross.cmake ..
+make
